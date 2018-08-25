4a8510968b84713471cb40222ded4fa8d9717aa5
조경대 <kyungdae.cho@navercorp.com>
2018-08-25 11:34:30 +0900

    33

--- src/main/java/me/caru/jpastudy/post/PostRepository.java
+++ src/main/java/me/caru/jpastudy/post/PostRepository.java
@@ -1,7 +1,13 @@
 package me.caru.jpastudy.post;
 
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 
 import me.caru.jpastudy.repository.CustomJpaRepository;
 
@@ -16,4 +22,9 @@ public interface PostRepository extends CustomJpaRepository<Post, Long> {
 	Page<Post> findByTitleContains(String title, Pageable pageable);
 
 	long countByTitleContains(String title);
+
+	List<Post> findByTitleStartingWith(@NotBlank String title);
+
+	@Query("SELECT p FROM Post as p where p.title = ?1")
+	List<Post> findByTitle(@NotBlank String title, Sort sort);
 }
